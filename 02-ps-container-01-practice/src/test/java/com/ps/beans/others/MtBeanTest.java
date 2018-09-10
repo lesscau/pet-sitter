d7011bdf56500a3315756112df7c0714998d0e0a
Olga Goremykina <lesscau@gmail.com>
2018-09-10 22:51:42 +0300

    02-ps-container-01-solution: TODO6
    TODO 6. Modify this class to use the new set of configuration files, created by resolving TODO 5.

--- 02-ps-container-01-practice/src/test/java/com/ps/beans/others/MtBeanTest.java
+++ 02-ps-container-01-practice/src/test/java/com/ps/beans/others/MtBeanTest.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 /**
@@ -17,12 +18,15 @@ public class MtBeanTest {
 
     @Test
     public void testConfig() {
-        //TODO 6. Modify this class to use the new set of configuration files, created by resolving TODO 5.
         //TODO 7. Try to use wildcards as well.
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:spring/others/sample-config-01.xml");
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(
+                "classpath:spring/others/collection-config.xml",
+                "classpath:spring/others/date-converter-config.xml",
+                "classpath:spring/others/factory-config.xml",
+                "classpath:spring/others/multiple-types-config.xml"
+        );
 
         MultipleTypesBean mtBean = (MultipleTypesBean) ctx.getBean("mtBean");
         assertNotNull(mtBean);
-
     }
 }
